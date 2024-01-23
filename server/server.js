aee91b2be310fe3e4c35475a258c9edc53478dab
Hunter Kam <hone648@gmail.com>
2024-01-23 08:04:56 +0000

    Added crud for users

--- server/server.js
+++ server/server.js
@@ -1,6 +1,63 @@
+require('./mongoose');
 const express = require('express');
 const app = express();
+const User = require('./models/user');
 
+app.use(express.json());
 
+app.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+});
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404);
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) {
+            return res.status(404);
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404);
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.listen(5000, () => { console.log("Server started on port 5000") })
