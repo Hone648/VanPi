70bf8dcd078b7943131c1cd15e974ff986942130
Hunter Kam <hone648@gmail.com>
2024-06-15 05:14:04 +0000

    added queryClient to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,13 @@ import { useState } from 'react';
 import Settings from './scenes/settings';
 import { configurationData } from './data/config';
 import CellAntenna from './scenes/cellBooster';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -27,56 +34,59 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app">
-          <Sidebar
-            isCollapsed={isCollapsed}
-            setIsCollapsed={setIsCollapsed}
-            selected={selected}
-            setSelected={setSelected}
-          />
-          <main className="content">
-            <TopBar selected={selected} setSelected={setSelected} />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route
-                path="/batteries"
-                element={
-                  <Batteries
-                    isCollapsed={isCollapsed}
-                    alternator={alternator}
-                  />
-                }
-              />
-              <Route
-                path="/tanks"
-                element={<Tanks isCollapsed={isCollapsed} />}
-              />
-              <Route
-                path="/settings"
-                element={
-                  <Settings
-                    data={configurationData}
-                    alternator={alternator}
-                    setAlternator={setAlternator}
-                  />
-                }
-              />
-              <Route
-                path="/notifications"
-                element={
-                  <Notifications notifications={notifications} />
-                }
-              />
-              <Route
-                path="/cellbooster"
-                element={
-                  <CellAntenna isCollapsed={isCollapsed}
-                  />
-                }
-              />
-            </Routes>
-          </main>
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <div className="app">
+            <Sidebar
+              isCollapsed={isCollapsed}
+              setIsCollapsed={setIsCollapsed}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <main className="content">
+              <TopBar selected={selected} setSelected={setSelected} />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route
+                  path="/batteries"
+                  element={
+                    <Batteries
+                      isCollapsed={isCollapsed}
+                      alternator={alternator}
+                    />
+                  }
+                />
+                <Route
+                  path="/tanks"
+                  element={<Tanks isCollapsed={isCollapsed} />}
+                />
+                <Route
+                  path="/settings"
+                  element={
+                    <Settings
+                      data={configurationData}
+                      alternator={alternator}
+                      setAlternator={setAlternator}
+                    />
+                  }
+                />
+                <Route
+                  path="/notifications"
+                  element={
+                    <Notifications notifications={notifications} />
+                  }
+                />
+                <Route
+                  path="/cellbooster"
+                  element={
+                    <CellAntenna isCollapsed={isCollapsed}
+                    />
+                  }
+                />
+              </Routes>
+            </main>
+          </div>
+        </QueryClientProvider>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
