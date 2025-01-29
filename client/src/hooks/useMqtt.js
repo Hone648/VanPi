d126f93345c0f8d2cded0a8ed617fcccf0ae405d
Hunter Kam <hone648@gmail.com>
2025-01-29 03:11:04 +0000

    simplified usemqtt hook

--- client/src/hooks/useMqtt.js
+++ client/src/hooks/useMqtt.js
@@ -1,25 +1,8 @@
-import React, { useEffect } from 'react';
-import { useMqtt } from '../context/MqttContext'; // Import the custom hook
+import { useContext } from 'react';
+import { MqttContext } from '../context/MqttContext';
 
-const MyComponent = () => {
-    const mqttClient = useMqtt(); // Get the MQTT client from the context
-
-    useEffect(() => {
-        if (mqttClient) {
-            // You can interact with the MQTT client here
-            mqttClient.subscribe('antenna/control', (err) => {
-                if (err) {
-                    console.log('Error subscribing to topic:', err);
-                }
-            });
-
-            mqttClient.on('message', (topic, message) => {
-                console.log('Received message:', message.toString());
-            });
-        }
-    }, [mqttClient]);
-
-    return <div>BoonDocker Hub MQTT Connected</div>;
+const useMqtt = () => {
+    return useContext(MqttContext);
 };
 
-export default MyComponent;
+export default useMqtt;
